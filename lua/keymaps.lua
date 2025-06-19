bb8672af87424423354e35a07917c9919345507c
Eric <32179572+ericstacks@users.noreply.github.com>
2025-06-19 15:26:08 -0300

    Update keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,6 @@
 -- Atalhos do modo insert
 vim.keymap.set("i", "<C-k>", "<C-o>D", { noremap = true })
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Listar buffers abertos" })
+vim.keymap.set('v', '<leader>ca', vim.lsp.buf.code_action, { noremap = true, silent = true, desc = "LSP Code Action (Visual)" })
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { noremap = true, silent = true, desc = "LSP Code Action" })
 
