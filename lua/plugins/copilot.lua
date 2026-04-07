3ea0d67a4acbfbfa18985e27b1ef98ab2d97a318
ericluciano <ericluciano.rj@gmail.com>
2026-04-07 12:19:45 -0300

    fix atalho neovim

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,18 +5,40 @@ return {
     cmd = "Copilot",          -- carrega sob demanda
     event = "InsertEnter",    -- ou "VeryLazy"
     config = function()
+      local suggestion = require("copilot.suggestion")
+
       require("copilot").setup({
         panel = { enabled = false }, -- painel flutuante opcional
         suggestion = {
           auto_trigger = true,
           keymap = {
-            accept = "<C-l>",   -- aceita sugestão
-            next   = "<C-.>",   -- próxima
-            prev   = "<C-,>",   -- anterior
-            dismiss= "<C-/>",   -- descarta
+            next   = "<M-]>",   -- próxima
+            prev   = "<M-[>",   -- anterior
+            dismiss= "<C-x>",   -- descarta
           },
         },
       })
+
+      vim.keymap.set("i", "<Tab>", function()
+        if vim.fn.pumvisible() == 1 then
+          return "<C-n>"
+        end
+
+        if suggestion.is_visible() then
+          suggestion.accept()
+          return ""
+        end
+
+        return "<Tab>"
+      end, { expr = true, silent = true, replace_keycodes = false, desc = "Aceita Copilot ou avanca no menu" })
+
+      vim.keymap.set("i", "<S-Tab>", function()
+        if vim.fn.pumvisible() == 1 then
+          return "<C-p>"
+        end
+
+        return "<S-Tab>"
+      end, { expr = true, silent = true, replace_keycodes = false, desc = "Volta no menu de completions" })
     end,
   },
 
